77517bb05e4adb58a04578db998f8bd17505473f
Jiatong (Kevin) Liang <61712729+Jiatong-Liang@users.noreply.github.com>
2025-09-29 08:28:34 -0400

    Update sfs_loglik.py to include projection

--- src/demesinfer/loglik/sfs_loglik.py
+++ src/demesinfer/loglik/sfs_loglik.py
@@ -11,3 +11,14 @@ def sfs_loglik(afs, esfs, sequence_length=None, theta=None):
         return jnp.sum(-tmp + xlogy(afs, tmp))
     else:
         return jnp.sum(xlogy(afs, esfs/esfs.sum()))
+
+def projection_sfs_loglik(afs, tp, proj_dict, einsum_str, input_arrays, sequence_length=None, theta=None):
+    proj_esfs = tp(proj_dict)
+    proj_afs = jnp.einsum(einsum_str, *input_arrays)
+
+    if theta:
+        assert(sequence_length)
+        tmp = proj_esfs * sequence_length * theta
+        return jnp.sum(-tmp + xlogy(proj_afs, tmp))
+    else:
+        return jnp.sum(xlogy(proj_afs, proj_esfs/jnp.sum(proj_esfs)))
