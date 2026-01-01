77d839ae8f0f44e104159ad09061650323d8fc80
Jiatong <jkliang@umich.edu>
2026-01-01 01:49:52 -0500

    testing api update

--- src/demesinfer/loglik/sfs_loglik.py
+++ src/demesinfer/loglik/sfs_loglik.py
@@ -31,16 +31,22 @@ def sfs_loglik(afs, esfs, sequence_length=None, theta=None):
     Notes
     -----
     In tskit, given a tree sequence, to obtain the afs one can use the function::
-        tree_sequence.allele_frequency_spectrum()
+        afs = tree_sequence.allele_frequency_spectrum(*options)
     
     To obtain the esfs, with ``momi3`` one must first initialize an ExpectedSFS object
-    with a demographic model and a dictionary of the number of samples used per population. 
+    with a ``demes`` demographic model and a dictionary of the number of samples used per population. 
     Then one would input a dictionary of parameter values into the Expected SFS object::
-        ESFS = demesinfer.sfs.ExpectedSFS(demes_model.to_demes(), num_samples=samples_per_population)
+        ESFS_obj = demesinfer.sfs.ExpectedSFS(demes_model.to_demes(), num_samples=samples_per_population)
         params = {param_key: value}
-        esfs = ESFS(params)
+        esfs = ESFS_obj(params)
 
-    Please refer to the tutorial for a specfic example.
+        multinomial_loglik_value = sfs_loglik(afs, esfs)
+        poisson_loglik_value = sfs_loglik(afs, esfs, sequence_length=1e8, theta=1e-8)
+
+    To compute the gradient, one can use ``jax.grad`` or ``jax.value_and_grad``. 
+    All loglikelihood functions are compatible with ``jax``.
+    
+    Please refer to the tutorial for a specfic example, the above provided codes are just outlines of how to call on the functions.
     
     See Also
     --------
@@ -57,6 +63,65 @@ def sfs_loglik(afs, esfs, sequence_length=None, theta=None):
         return jnp.sum(xlogy(afs, esfs/esfs.sum()))
 
 def projection_sfs_loglik(afs, tp, proj_dict, einsum_str, input_arrays, sequence_length=None, theta=None):
+    """
+    Log-likelihood for projected site frequency spectrum.
+    
+    Computes Poisson(λ) or multinomial likelihood comparing observed AFS
+    to expected SFS projected via Einstein summation.
+
+    This function evaluates the **projected** multinomial or Poisson log-likelihood of an
+    observed site frequency spectrum (AFS) given an expected spectrum (ESFS).
+
+    By default, the sequence length and mutation rate (theta) are None, indicating
+    that the multinomial likelihood will be used. To use the Poisson likelihood, one must
+    provide BOTH the sequence length and mutation rate (theta).
+
+    Parameters
+    ----------
+    esfs_obj : array_like
+        An demesinfer.sfs.ExpectedSFS object
+    sequence_length : int, optional
+        Total number of sites in the sequence. Required if ``theta`` is given
+    theta : float, optional
+        Population-scaled mutation rate. If provided, a sequence length must also 
+        be provided and the Poisson likelihood is used, 
+        otherwise a multinomial likelihood is assumed. 
+    proj_dict : dict 
+        Dictionary of arrays that represent projection vectors
+    einsum_str : string 
+        Einstein summation string for projection
+    input_arrays: array_like
+        Input arrays for einsum operation, it must contain the original afs
+
+    Returns
+    -------
+    float
+        Log-likelihood of the projected observed spectrum given the projected expected spectrum.
+
+    Notes
+    -----
+    proj_dict contains the random projection vectors that define the low-dimensional 
+    subspace for approximating the full expected SFS, einsum_str is a string specifying 
+    the Einstein summation for tensor operations, and input_arrays are preprocessed arrays 
+    that serve as inputs to the jax.numpy.einsum call, optimized for JAX’s just-in-time compilation
+
+    Example::
+        proj_dict, einsum_str, input_arrays = prepare_projection(afs, afs_samples, sequence_length, num_projections, seed)
+        esfs_obj = ExpectedSFS(demo.to_demes(), num_samples=afs_samples)
+        params = {param_key: val}
+        projection_sfs_loglik(esfs_obj, params, proj_dict, einsum_str, input_arrays, sequence_length=None, theta=None)
+    
+    Internally this function will call on demesinfer.sfs.ExpectedSFS.tensor_prod, which performs the projection
+    operations on the site frequency spectrum.
+    
+    Please refer to the tutorial for a specfic example, the above provided codes are just outlines of how to call on the functions.
+    
+    See Also
+    --------
+    demesinfer.sfs.ExpectedSFS
+    demesinfer.sfs.ExpectedSFS.tensor_prod
+    demesinfer.sfs.sfs_loglik.prepare_projection
+    """
     proj_esfs = tp(proj_dict)
     proj_afs = jnp.einsum(einsum_str, *input_arrays)
 
